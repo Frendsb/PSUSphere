9b0342258469017f35ed2433f30e969f4dd634d8
Frendsb <202380372@psu.palawan.edu.ph>
2025-09-14 23:23:57 +0800

    changes

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     BASE_DIR / "static",
-]
+)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
