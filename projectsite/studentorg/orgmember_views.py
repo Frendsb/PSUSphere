63f9f92c227cf610d45a958f82446b0d3c8ea964
Frendsb <202380372@psu.palawan.edu.ph>
2025-09-21 21:42:18 +0800

    fixing search function in organization members

--- projectsite/studentorg/orgmember_views.py
+++ projectsite/studentorg/orgmember_views.py
@@ -3,12 +3,25 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import OrgMember
 from django.urls import reverse_lazy
 from .forms import OrganizationForm
+from django.db.models import Q
 
 class OrgMemberList(ListView):
     model = OrgMember
     context_object_name = 'orgmembers'
     template_name = 'orgmem_list.html'
     paginate_by = 10
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(student__lastname__icontains=query) |
+                Q(student__firstname__icontains=query) |
+                Q(student__middlename__icontains=query) |
+                Q(organization__name__icontains=query)
+            )
+        return qs
 
 class OrgMemberCreateView(CreateView):
     model = OrgMember
