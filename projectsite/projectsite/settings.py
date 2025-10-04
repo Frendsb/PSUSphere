ccdd07e6966a553e0330383a017ff0dcf7167a02
Frendsb <202380372@psu.palawan.edu.ph>
2025-10-04 20:00:41 +0800

    allowed hosts in settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ls@ekmdpk50$hyn2d(m@mmp!om88b!+@p87s_*ubn5_bjud=gv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['efren2.pythonanywhere.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'efren2.pythonanywhere.com']
 
 # Application definition
 
